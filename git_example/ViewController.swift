0a63ec83852b27fdefff70ef2faa1a434e65a072
Armen <gasparyana2018@gmail.com>
2019-06-30 19:25:07 +0400

    Initial Commit

---
+++ git_example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git_example
+//
+//  Created by Armen Gasparyan on 6/30/19.
+//  Copyright © 2019 SLTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
